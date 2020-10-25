77203f7648a63b62317ccbc11e609a2d84a9bf6b
ShaneMraz <shane@mraz.io>
2020-10-24 19:50:37 -0700

    loop for drop downs

--- cypress/integration/events.spec.js
+++ cypress/integration/events.spec.js
@@ -4,7 +4,7 @@
 // wait for page to load
 // validate something on the page exists
 
-context('Home Page', () => {
+context('Events Scenario', () => {
     beforeEach(() => {
         cy.acceptCookies();
         
@@ -14,38 +14,67 @@ context('Home Page', () => {
         // cy.wait('@apiP').its('status').should('be', 200)
     })
 
-    xit('Fullscreen - Verifies Events', () => {
-        cy.waitUntil(() => cy.get('.znv-z-full-nav').should('be.visible'));
+    describe('Fullscreen - Events', () => {
+        it('Verifies Cycling', () => {
+            cy.waitUntil(() => cy.get('.znv-z-full-nav').should('be.visible'));
+    
+            cy.get('.znv-d-lg-flex > [href="/events"]').click()
+    
+            cy.waitUntil(() => cy.get('div#zwift-events').should('be.visible'))
+            cy.url().should('include', '/events')
+    
+            cy.get(':nth-child(1) > .select-wrapper > #filter-location').select('CYCLING').then(() => {
+                cy.waitUntil(() => cy.contains('CYCLING').should('be.visible'))
 
-        cy.get('.znv-d-lg-flex > [href="/events"]').click()
+                cy.get(':nth-child(2) > .select-wrapper > #filter-location').find('option').then(types => {
+                    for (let type = 2;  type < types.length; type++) {
+                        cy.get(':nth-child(2) > .select-wrapper > #filter-location').select(types[type].label)
+                    }
+                })
+            });
+        });
 
-        cy.waitUntil(() => cy.get('div#zwift-events').should('be.visible'))
-        cy.url().should('include', '/events')
+        xit('Verifies Running', () => {
+            cy.waitUntil(() => cy.get('.znv-z-full-nav').should('be.visible'));
+    
+            cy.get('.znv-d-lg-flex > [href="/events"]').click()
+    
+            cy.waitUntil(() => cy.get('div#zwift-events').should('be.visible'))
+            cy.url().should('include', '/events')
 
-        cy.get(':nth-child(1) > .select-wrapper > #filter-location').select('CYCLING')
-        cy.contains('CYCLING').should('be.visible')
+            cy.get(':nth-child(1) > .select-wrapper > #filter-location').select('RUNNING').then(() => {
+                cy.waitUntil(() => cy.contains('RUNNING').should('be.visible'));
 
-        cy.get(':nth-child(1) > .select-wrapper > #filter-location').select('RUNNING')
-        cy.contains('RUNNING').should('be.visible')
+                cy.get(':nth-child(2) > .select-wrapper > #filter-location').find('option').then(types => {
+                    for (let type = 2;  type < types.length; type++) {
+                        cy.get(':nth-child(2) > .select-wrapper > #filter-location').select(types[type].label)
+                    }
+                })
+            });
+        });
     })
+    
 
-    it('Mobile - Verifies Events', () => {
+    xit('Mobile - Verifies Events', () => {
         cy.viewport('iphone-x')
+
         cy.waitUntil(() => cy.get('#znv-header-open-burger').should('be.visible'));
 
-        cy.get('#znv-header-open-burger').click()
+        cy.get('#znv-header-open-burger').click();
 
-        cy.waitUntil(() => cy.get(':nth-child(10) > .znv-link-black').should('be.visible'))
+        cy.waitUntil(() => cy.get(':nth-child(10) > .znv-link-black').should('be.visible')).then(() => {
+            cy.get(':nth-child(10) > .znv-link-black').click()
+        });
 
-        cy.get(':nth-child(10) > .znv-link-black').click()
+        cy.waitUntil(() => cy.get('#events-header').should('be.visible'));
 
-        cy.waitUntil(() => cy.get('#events-header').should('be.visible'))
+        cy.get(':nth-child(1) > .select-wrapper > #filter-location').select('CYCLING').then(() => {
+            cy.waitUntil(() => cy.contains('Cycling').should('be.visible'))
+        });
 
-        cy.get(':nth-child(1) > .select-wrapper > #filter-location').select('CYCLING')
-        cy.contains('Cycling').should('be.visible')
-
-        cy.get(':nth-child(1) > .select-wrapper > #filter-location').select('RUNNING')
-        cy.contains('Running').should('be.visible')
+        cy.get(':nth-child(1) > .select-wrapper > #filter-location').select('RUNNING').then(() => {
+            cy.waitUntil(() => cy.contains('Running').should('be.visible'))
+        })  
     })
 })
 
